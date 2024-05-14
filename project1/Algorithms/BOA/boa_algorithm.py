61877f98664805f4aca4fc4d5cb81fad24f13430
kwieciak <bkwiecinski02@gmail.com>
2024-05-14 14:52:00 +0200

    feat: implemented boa algorithm with levy flight and some new features

--- project1/Algorithms/BOA/boa_algorithm.py
+++ project1/Algorithms/BOA/boa_algorithm.py
@@ -1,4 +1,5 @@
 import copy
+import math
 import random
 
 import numpy as np
@@ -17,9 +18,9 @@ class BoaAlgorithm:
         self.best_adaptation = float('inf')
         self.best_butterfly = None
         self.best_values_over_time = []
-        self.c = c
-        self.a = a
-        self.p = p
+        self.c = c #senson modality
+        self.a = a #power exponent
+        self.p = p #probability
 
     def optimize(self):
         for i in range(self.iterations):
@@ -30,11 +31,48 @@ class BoaAlgorithm:
             for j in range(len(self.population)):
                 r = random.uniform(0, 1)
                 if r < self.p:
-                    self.population[j].vector = self.add_vectors(self.population[j].vector, self.multiply_vector_by_scalar((self.substraction_vectors(self.multiply_vector_by_scalar(self.best_butterfly.vector, r*r), self.population[j].vector)), self.population[j].fragrance))
+                    self.population[j].vector = self.add_vectors(self.population[j].vector, self.multiply_vector_by_scalar(
+                        (self.substraction_vectors(self.multiply_vector_by_scalar(self.best_butterfly.vector, r*r),
+                                                   self.population[j].vector)), self.population[j].fragrance))
                 else:
                     rand_but_1, rand_but_2 = self.sample(2, j, len(self.population))
-                    self.population[j].vector = self.add_vectors(self.population[j].vector, self.multiply_vector_by_scalar((self.substraction_vectors(self.multiply_vector_by_scalar(rand_but_1.vector, r*r), rand_but_2.vector)), self.population[j].fragrance))
+                    self.population[j].vector = self.add_vectors(self.population[j].vector, self.multiply_vector_by_scalar(
+                        (self.substraction_vectors(self.multiply_vector_by_scalar(rand_but_1.vector, r*r), rand_but_2.vector)),
+                        self.population[j].fragrance))
             self.a = 0.1 + (0.2 * (i / self.iterations))
+            self.best_values_over_time.append(self.best_adaptation)
+        return self.best_adaptation, self.best_values_over_time
+
+    def optimize_with_levy_flight(self):
+        for i in range(self.iterations):
+            for j in range(len(self.population)):
+                self.population[j].fragrance = self.calculate_f(self.c, self.calculate_adaptation(self.population[j]),
+                                                                self.a) / self.best_adaptation + 2.2204e-16
+            self.update_adaptation()
+            for j in range(len(self.population)):
+                r = random.uniform(0, 1)
+                if r < self.p:
+                    step = self.levy_flight()
+                    local_search_vector = self.multiply_vectors(
+                        self.substraction_vectors(self.best_butterfly.vector, self.population[j].vector), step)
+                    self.population[j].vector = self.add_vectors(self.population[j].vector,
+                                                                 self.multiply_vector_by_scalar(local_search_vector,
+                                                                                                self.population[j].fragrance))
+                else:
+                    rand_but_1, rand_but_2 = self.sample(2, j, len(self.population))
+                    M1 = self.mutation(rand_but_1.vector, rand_but_2.vector)
+                    K1 = self.crossover(M1, self.population[j].vector)
+                    rand_but_3, rand_but_4 = self.sample(2, j, len(self.population))
+                    M2 = self.mutation(rand_but_3.vector, rand_but_4.vector)
+                    K2 = self.crossover(M2, self.population[j].vector)
+                    step = self.levy_flight()
+                    global_search_vector = self.multiply_vectors(
+                        self.substraction_vectors(K1, K2), step)
+                    self.population[j].vector = self.add_vectors(self.population[j].vector,
+                                                                 self.multiply_vector_by_scalar(global_search_vector,
+                                                                                                self.population[j].fragrance))
+            self.a = 0.1 + (0.2 * (i / self.iterations))
+            self.c = 1 - 0.6 * math.sqrt(i / self.iterations)
             self.best_values_over_time.append(self.best_adaptation)
         return self.best_adaptation, self.best_values_over_time
 
@@ -45,7 +83,7 @@ class BoaAlgorithm:
             butterflies.append(new_butterfly)
         return butterflies
 
-    def calculate_f(self, c, i, a):
+    def calculate_f(self, c, i, a): #i = intensity, f = fragrance
         return c * (i ** a)
 
     def calculate_adaptation(self, butterfly):
@@ -76,4 +114,28 @@ class BoaAlgorithm:
     def add_vectors(self, vector1, vector2):
         return [x + y for x, y in zip(vector1, vector2)]
 
+    def multiply_vectors(self, vector1, vector2):
+        return [x * y for x, y in zip(vector1, vector2)]
 
+    def levy_flight(self):
+        lambda_sign = 1.5
+        sigma = (np.math.gamma(1 + lambda_sign) * np.sin(np.pi * lambda_sign / 2) / (
+                np.math.gamma((1 + lambda_sign) / 2) * lambda_sign ** (2 * ((lambda_sign - 1) / 2)))) ** (
+                        1 / lambda_sign)
+        u = np.random.normal(0, sigma, self.vector_size)
+        v = np.random.standard_normal(self.vector_size)
+        epsilon = 1e-10
+        step = u / (np.abs(v) ** (1 / lambda_sign) + epsilon)
+        return 0.1 * step
+
+    def crossover(self, m, v):
+        new = v.copy()
+        for i in range(len(v)):
+            if random.uniform(0, 1) < 0.7:
+                new[i] = m[i]
+        return new
+
+    def mutation(self, d, e):
+        new = [self.best_butterfly.vector[i] + 0.5 * (d[i] - e[i]) for i in range(self.vector_size)]
+        new = [min(max(val, self.left_border), self.right_border) for val in new]
+        return new
--- project1/main.py
+++ project1/main.py
@@ -84,29 +84,76 @@ if __name__ == '__main__':
     # plt.plot(range(1, len(best_solutions) + 1), best_solutions, label='BAT', color='blue')
     # plt.show()
 
-    time_start = time()
-    BOA = boa_algorithm.BoaAlgorithm(100, 20, -100, 100, 1000, functions.sphere)
-    best_adapt, best_solutions = BOA.optimize()
-    time_end = time()
-
-    print("BOA Value:", best_adapt, " Time: ", time_end - time_start)
-    plt.plot(range(1, len(best_solutions) + 1), best_solutions, label='BOA', color='blue')
-    plt.show()
+    # time_start = time()
+    # BOA = boa_algorithm.BoaAlgorithm(100, 20, -100, 100, 1000, functions.sphere)
+    # best_adapt, best_solutions = BOA.optimize_with_levy_flight()
+    # time_end = time()
+    #
+    # print("BOA Value:", best_adapt, " Time: ", time_end - time_start)
+    # plt.plot(range(1, len(best_solutions) + 1), best_solutions, label='BOA', color='blue')
+    # plt.show()
     #print("DE Value:", best_solutions8[-1], " Time: ", time8)
 
-    # def plot(scores, scores2):
-    #     plt.figure(figsize=(10, 6))
+    time_start1 = time()
+    BAT1 = bat_algorithm.BatAlgorithm(25, 30, -100, 100, 1000, 0, 10, functions.griewank_function)
+    best_adapt1, best_solutions1 = BAT1.optimize()
+    time_end1 = time()
+
+    # time_start2 = time()
+    # BAT2 = bat_algorithm.BatAlgorithm(25, 30, -100, 100, 1000, 0, 10, functions.sphere)
+    # best_adapt2, best_solutions2 = BAT2.optimize()
+    # time_end2 = time()
     #
-    #     plt.plot(range(1, len(scores) + 1), scores, label='PSO_DE ', color='green')
-    #     plt.plot(range(1, len(scores2) + 1), scores2, label='DE', color='red')
+    # time_start3 = time()
+    # BAT3 = bat_algorithm.BatAlgorithm(25, 30, -100, 100, 1000, 0, 20, functions.sphere)
+    # best_adapt3, best_solutions3 = BAT3.optimize()
+    # time_end3 = time()
+    # print("frequency = [0,5]  f(x)=", best_adapt1, " Time: ", time_end1 - time_start1)
+    # print("frequency = [0,10]  f(x)=", best_adapt2, " Time: ", time_end2 - time_start2)
+    # print("frequency = [0,20]  f(x)=", best_adapt3, " Time: ", time_end3 - time_start3)
+
+    time_start2 = time()
+    boa1 = boa_algorithm.BoaAlgorithm(75, 30, -100, 100, 1000, functions.griewank_function)
+    best_adapt2, best_solutions2 = boa1.optimize_with_levy_flight()
+    time_end2 = time()
+
+    time_start3 = time()
+    boa1 = boa_algorithm.BoaAlgorithm(75, 30, -100, 100, 1000, functions.griewank_function)
+    best_adapt3, best_solutions3 = boa1.optimize()
+    time_end3 = time()
+
+    # time_start2 = time()
+    # boa2 = boa_algorithm.BoaAlgorithm(75, 30, -100, 100, 1000, functions.sphere, c=0.1)
+    # best_adapt2, best_solutions2 = boa2.optimize_with_levy_flight()
+    # time_end2 = time()
     #
-    #     plt.xlabel('Iteration')
-    #     plt.ylabel('GRIEWANK FUNCTION VALUE')
-    #     plt.yscale('log')
-    #     plt.legend()
-    #     plt.grid(True)
-    #     plt.show()
-    #
+    # time_start3 = time()
+    # boa3 = boa_algorithm.BoaAlgorithm(75, 30, -100, 100, 1000, functions.sphere, c=1)
+    # best_adapt3, best_solutions3 = boa3.optimize_with_levy_flight()
+    # time_end3 = time()
+    # print("sensor modality = 0.01   f(x)=", best_adapt1, " Time: ", time_end1 - time_start1)
+    # print("sensor modality = 0.1  f(x)=", best_adapt2, " Time: ", time_end2 - time_start2)
+    # print("sensor modality = 1  f(x)=", best_adapt3, " Time: ", time_end3 - time_start3)
+
+    print("BAT Value:", best_adapt1, " Time: ", time_end1 - time_start1)
+    print("BOA Value with Levy Flight:", best_adapt2, " Time: ", time_end2 - time_start2)
+    print("BOA Value without Levy Flight:", best_adapt3, " Time: ", time_end3 - time_start3)
+
+    def plot(scores, scores2, scores3):
+        plt.figure(figsize=(10, 6))
+
+        plt.plot(range(1, len(scores) + 1), scores, label='BAT  ', color='green')
+        plt.plot(range(1, len(scores2) + 1), scores2, label='BOA with Levy Flight ', color='red')
+        plt.plot(range(1, len(scores3) + 1), scores3, label='BOA without Levy Flight ', color='blue')
+
+        plt.xlabel('Iteration')
+        plt.ylabel('GRIEWANK FUNCTION VALUE')
+        plt.yscale('log')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    plot(best_solutions1, best_solutions2, best_solutions3)
     # # plot(best_solutions1, best_solutions2, best_solutions3)
     # # plot(best_solutions4, best_solutions5, best_solutions6)
     #plot(best_solutions7, best_solutions8)
